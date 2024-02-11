1c3c1c85334cab8d7d4a6b5242c8c076b82e8027
Shakeel Ansari <ShakeelAnsari63@Gmail.com>
2024-02-11 20:44:37 +0530

    New Flutter App instead of Flet

--- main.py
+++
@@ -1,5 +0,0 @@
-import flet as ft
-from pages.main import MainPage
-
-
-ft.app(MainPage)
--- pages/main.py
+++
@@ -1,7 +0,0 @@
-import flet as ft
-
-
-def MainPage(page: ft.Page):
-    page.vertical_alignment = ft.MainAxisAlignment.CENTER
-    page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
-    page.add(ft.SafeArea(ft.Text("Hello, Flet!")))
